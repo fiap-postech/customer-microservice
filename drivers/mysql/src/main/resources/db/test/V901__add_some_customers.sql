37af61da9f8cd2e4b2bc09e66ab878a1bcd0d66e
Eberson Oliveira <eberson.olivera@gmail.com>
2023-12-29 21:29:17 -0300

    feat: added some customers for test purposes

---
+++ drivers/mysql/src/main/resources/db/test/V901__add_some_customers.sql
@@ -0,0 +1,5 @@
+insert into customer (id, uuid, document, name, email, enabled, created, last_updated, version)
+values (id, UUID(), '76633036876', 'Luna Pietra Heloisa Drumond', 'luna_pietra_drumond@cppcoder.com', 1, now(), now(), 0),
+       (id, UUID(), '65511960802', 'Camila Louise da Cunha', 'camila.louise.dacunha@homail.com', 1, now(), now(), 0),
+       (id, UUID(), '89093796852', 'Pietro Erick dos Santos', 'pietro_erick_dossantos@afsn.com.br', 1, now(), now(), 0),
+       (id, UUID(), '32495281885', 'Juan Luan Danilo Fernandes', 'juan.luan.fernandes@click21.com.br', 1, now(), now(), 0);
